de4ca2d87db11f88fcaee65926f6bff63907fb9b
Siva <linga.lab.5.19.2018@gmail.com>
2021-04-28 16:48:24 +0530

    future event added

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -22,6 +22,14 @@ class EventsController extends BaseController
     }
 
     public function getFutureEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 2');
+
+        $events = DB::table('events')->join('workshops', function ($join) {
+            $join->on('events.id', '=', 'workshops.event_id')
+                 ->where('workshops.end', '>',  date('Y-m-d').' 00:00:00');
+        } )->get();
+//'workshops', 'events.id', '=', 'workshops.event_id'
+               
+        return $events->toArray();
+        //throw new \Exception('implement in coding task 2');
     }
 }
