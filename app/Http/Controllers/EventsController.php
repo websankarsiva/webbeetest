3d509ead7dd7808693c41e9cc1e9e3ae30dc0ab9
Siva <linga.lab.5.19.2018@gmail.com>
2021-04-28 16:15:45 +0530

    eventwithworkshop func added

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -8,11 +8,17 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 
 class EventsController extends BaseController
 {
     public function getEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 1');
+
+        $events = DB::table('events')->get();
+
+        return $events->toArray();
+
+        //throw new \Exception('implement in coding task 1');
     }
 
     public function getFutureEventsWithWorkshops() {
